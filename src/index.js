7fe84c27283ab9f74adec2160ee2feb98f3d0935
sammyPro <samuel295@gmail.com>
2021-11-27 21:27:16 -0400

    Cleaning the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-//Words for the phrase
-// const msgStart = [ 'Be Yourself', 'Dream Bird', 'Rise Above', 'You Matter'];
-// const msgMiddle = [ 'You Can', 'Accept Yourself', 'Trust Yourself' ];
-// const msgEnd = [ 'Consistency=Success', 'Stay Focused','Move Forward','Try Again'];
-// const getRandomPhrase = phrases => phrases[Math.floor(Math.random() * phrases.length)];
-// const generateMessage = () => getRandomPhrase(msgStart) + ', ' + getRandomPhrase(msgMiddle) + ' & ' + getRandomPhrase(msgEnd);
-// console.log(generateMessage());
 
 //Create the message
 const MessagesGenerator = {
